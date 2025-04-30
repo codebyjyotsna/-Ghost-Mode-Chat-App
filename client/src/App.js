71537d8f86884678774d374437b12d031d946332
Jyotsna Thakur <j.thakur2608@gmail.com>
2025-04-30 22:59:38 +0530

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatRoom from "./components/ChatRoom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Ghost Mode Chat</h1>
+      <ChatRoom />
+    </div>
+  );
+};
+
+export default App;
